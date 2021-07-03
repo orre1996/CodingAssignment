2c96a962b6ad0921be723ce4f876031146816dcc
Oscar Berggren <oscar.berggren@accenture.com>
2021-07-03 10:59:53 +0200

    Pods set up

---
+++ Pods/Target Support Files/Pods-ElectroluxCodeAssignment-ElectroluxCodeAssignmentUITests/Pods-ElectroluxCodeAssignment-ElectroluxCodeAssignmentUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ElectroluxCodeAssignment_ElectroluxCodeAssignmentUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ElectroluxCodeAssignment_ElectroluxCodeAssignmentUITests
+@end
