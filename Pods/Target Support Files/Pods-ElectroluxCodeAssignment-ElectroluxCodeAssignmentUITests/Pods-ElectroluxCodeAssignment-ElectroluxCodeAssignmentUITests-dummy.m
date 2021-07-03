d624f5082eee1d2179c15292d3ab511876671a7c
Oscar Berggren <oscar.berggren@accenture.com>
2021-07-03 14:23:59 +0200

    Removed pod files

--- Pods/Target Support Files/Pods-ElectroluxCodeAssignment-ElectroluxCodeAssignmentUITests/Pods-ElectroluxCodeAssignment-ElectroluxCodeAssignmentUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ElectroluxCodeAssignment_ElectroluxCodeAssignmentUITests : NSObject
-@end
-@implementation PodsDummy_Pods_ElectroluxCodeAssignment_ElectroluxCodeAssignmentUITests
-@end
--- Pods/Target Support Files/Pods-ElectroluxCodeAssignment/Pods-ElectroluxCodeAssignment-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ElectroluxCodeAssignment : NSObject
-@end
-@implementation PodsDummy_Pods_ElectroluxCodeAssignment
-@end
