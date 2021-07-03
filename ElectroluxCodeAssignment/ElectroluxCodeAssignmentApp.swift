d0a306f246ec68f5b64f1034b32297ff8c8674a9
Oscar Berggren <oscar.berggren@accenture.com>
2021-07-03 10:54:32 +0200

    Project set up

---
+++ ElectroluxCodeAssignment/ElectroluxCodeAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElectroluxCodeAssignmentApp.swift
+//  ElectroluxCodeAssignment
+//
+//  Created by Oscar Berggren on 2021-07-03.
+//
+
+import SwiftUI
+
+@main
+struct ElectroluxCodeAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
